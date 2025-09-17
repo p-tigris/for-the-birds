4e35eab7c76e8bb47539e6b88e692d89c926fa49
smdhar <shoumik_dhar@yahoo.com>
2025-09-17 03:13:09 -0400

    initial models created

--- birdlocations/settings.py
+++ birdlocations/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.postgres',
     'django.contrib.staticfiles',
 ]
 
@@ -75,8 +76,9 @@ WSGI_APPLICATION = 'birdlocations.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'birdlocations',
     }
 }
 
--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+
+RATINGS = (
+    (1, '1 star'),
+    (1.5, '1.5 stars'),
+    (2, '2 stars'),
+    (2.5, '2.5 stars'),
+    (3, '3 stars'),
+    (3.5, '3.5 stars'),
+    (4, '4 stars'),
+    (4.5, '4.5 stars'),
+    (5, '5 stars')
+)
 
 # Create your models here.
 class Location(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    tag = models.CharField(max_length=50)
+    description = models.TextField(max_length=800)
+    birds = ArrayField(models.CharField(max_length=50), default=list, size=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+
 
 class Review(models.Model):
-
+    title = models.CharField(max_length=50)
+    rating = models.FloatField(max_length=1, choices=RATINGS, default=RATINGS[0][0], null=True)
+    date = models.DateField(null=True)
+    text = models.TextField(max_length=800)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_title_display()}"
+    
